f44608778cf3274ee4b2fac8185063a146e4e47f
Md Ataullha <mdataullha@Mds-MacBook-Air.local>
2024-11-18 20:36:13 +0600

    2024-11.0.4, Obfuscated at 2024-11-18 06:34:05 on https://www.phpkobo.com/html-obfuscator

--- app.js
+++
@@ -1,8 +0,0 @@
-// Select the element where the year will be added
-const yearElement = document.getElementById('year');
-
-// Get the current year using the Date object
-const currentYear = new Date().getFullYear();
-
-// Set the text content of the element to the current year
-yearElement.textContent = currentYear;
